2f9f820eb9a9e3ce9235ded5314a7dc41074ade2
ibnefer <alexandrealcime3@gmail.com>
2025-11-04 16:37:50 -0500

    feat: add missing endpoints

---
+++ Domain/Controller/Private/Usuario/UsuarioControllerEditOneDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Domain.Controller.Private.Usuario
+{
+    public class UsuarioControllerEditOneDto
+    {
+        public string? Nombre { get; set; }
+        public string? UsuarioNombre { get; set; }
+        public string? Password { get; set; }
+        public bool? Activo { get; set; }
+    }
+}
